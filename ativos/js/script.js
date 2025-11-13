de4faeb99ffb188e71bd77ebe811f93e140c4e57
projeto-GuiaEstudo <projetoguiaestudoifpe@gmail.com>
2025-11-13 16:39:36 -0300

    versao01
    Adicionada a parte estrutural do projeto

---
+++ ativos/js/script.js
@@ -0,0 +1,24 @@
+// // Falta Fazer 
+// fetch('components/overlay')
+//     .then(response => response.text())
+//     .then(html => document.body.insertAdjacentHTML('beforeend', html));
+
+// // Falta Fazer 
+// fetch('components/overlay')
+//     .then(response => response.text())
+//     .then(html => document.body.insertAdjacentHTML('beforeend', html));
+
+// // Falta fazer
+// fetch('components/overlay')
+//     .then(response => response.text())
+//     .then(html => document.body.insertAdjacentHTML('beforeend', html));
+
+// // Falta Fazer 
+// fetch('components/overlay')
+//     .then(response => response.text())
+//     .then(html => document.body.insertAdjacentHTML('beforeend', html));
+
+// // Falta fazer
+// fetch('components/overlay')
+//     .then(response => response.text())
+//     .then(html => document.body.insertAdjacentHTML('beforeend', html));
