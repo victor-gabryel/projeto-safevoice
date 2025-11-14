8086820a93e615e26ee3bf95ceade13dbfd4fb1b
projeto-GuiaEstudo <projetoguiaestudoifpe@gmail.com>
2025-11-14 14:11:37 -0300

    versao01

--- ativos/js/script.js
+++ ativos/js/script.js
@@ -22,3 +22,21 @@
 // fetch('components/overlay')
 //     .then(response => response.text())
 //     .then(html => document.body.insertAdjacentHTML('beforeend', html));
+
+
+// Seção Dúvidas
+const faqQuestions = document.querySelectorAll(".faq-question");
+
+faqQuestions.forEach(btn => {
+    btn.addEventListener("click", () => {
+        btn.classList.toggle("active");
+
+        const answer = btn.nextElementSibling;
+
+        if (btn.classList.contains("active")) {
+            answer.style.maxHeight = answer.scrollHeight + "px";
+        } else {
+            answer.style.maxHeight = 0;
+        }
+    });
+});
